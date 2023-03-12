e7629b298e202d57a2df1a68a2523ab65cbffd84
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-03-12 20:28:22 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+// currentTimePlayer = 0;
+
+function onPlay() {
+  console.log('played the video!');
+
+  player.getCurrentTime().then(function (time) {
+    console.log('videoplayer-current-time:', time);
+    localStorage.setItem('videoplayer-current-time', time);
+    localStorage.getItem('videoplayer-current-time');
+  });
+  setTimeout(onPlay, 1000);
+}
+
+player.on('play', onPlay);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
