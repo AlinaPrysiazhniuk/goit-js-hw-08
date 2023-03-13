4f3963a469f7bc7a7db02d28a00a8219381b9ff0
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-03-14 00:33:00 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,19 +4,20 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 
 const updateTime = () => {
-  if (!localStorage.getItem('videoplayer-current-time')) {
-    player.getCurrentTime().then(function (time) {
-      localStorage.setItem('videoplayer-current-time', time);
-    });
-  } else {
-    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-  }
+  player.getCurrentTime().then(function (time) {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
 };
 
 player.on('timeupdate', throttle(updateTime, 1000));
 
+if (!localStorage.getItem('videoplayer-current-time')) {
+  localStorage.setItem('videoplayer-current-time', 0);
+  player.setCurrentTime(0);
+} else {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
-
-//console.log(localStorage.getItem('videoplayer-current-time'));
